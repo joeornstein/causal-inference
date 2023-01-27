be54ee0fd79eafda24de9c79d78e937d567c09cd
joeornstein <jornstein@gmail.com>
2023-01-27 13:32:19 -0500

    pre-treatment covariates

--- R/007_pretreatment_covariates.R
+++ R/007_pretreatment_covariates.R
@@ -5,7 +5,9 @@
 
 ## Simulate Tennessee STAR
 
-N <- 1e4 # 10,000 kids
+set.seed(1000)
+
+N <- 5e3 # 5,000 kids
 
 # randomly assigned to small or large class
 Tr <- sample(c(0,1), size = N, replace = TRUE)
@@ -13,9 +15,15 @@ Tr <- sample(c(0,1), size = N, replace = TRUE)
 # half of them are boys, half girls
 boy <- sample(c(0,1), size = N, replace = TRUE)
 
-# grades are affected by both the class size
-# treatment and gender, and a bunch of other things
-grade <- 0.2*Tr - 1*boy + rnorm(N, mean = 0, sd = 5)
+# half rich, half poor
+rich <- sample(c(0,1), size = N, replace = TRUE)
+
+# grades are affected by:
+# (1) the class size treatment
+# (2) gender
+# (3) family income
+# (4) and a bunch of other things we don't measure (rnorm)
+grade <- 0.1*Tr - 1*boy + 3*rich + rnorm(N)
 
 
 # this is an unbiased estimator of the treatment effect
@@ -28,13 +36,33 @@ summary(lm1)
 # which is why it has a relatively large standard error
 
 # the *precision* of the estimator is one rationale
-# for including some of those other things that affect the outcome
-# in our model
+# for including some of those other pre-treatment covariates
+# that affect the outcome in our model
 
-lm2 <- lm(grade ~ Tr + boy)
+lm2 <- lm(grade ~ Tr + boy + rich)
 summary(lm2)
 
 
+
+# graphically:
+
+library(tidyverse)
+
+# this is what we're doing when we don't condition on the
+# pre-treatment covariates
+ggplot(mapping = aes(x = grade,
+                     fill = factor(Tr))) +
+  geom_density(alpha = 0.5) +
+  labs(fill = 'Treatment Group')
+
+# this is what we're doing when we condition on
+# the pre-treatment covariates
+ggplot(data = tibble(grade, Tr, boy, rich),
+       mapping = aes(x = grade,
+                     fill = factor(Tr))) +
+  geom_density(alpha = 0.5) +
+  labs(fill = 'Treatment Group') +
+  facet_grid(boy~rich)
 
 
 
